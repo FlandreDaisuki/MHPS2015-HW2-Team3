f96607dfa1e3476222751c0b8c7952dca74acb8d
FlandreDaisuki <vbnm123c@gmail.com>
2015-05-10 09:10:53 +0800

    calc fitness before sort

--- main.cpp
+++ main.cpp
@@ -18,11 +18,32 @@ int main(int argc, char const *argv[])
     parent.calculateFitness();
     for (int pitr = 0; pitr < POPULATION_ITERATION; ++pitr)
     {
+        /*
+        When pitr == 0, Makespan and Fitness correct.
+        Here Fitness should be EXACTLY correct.
+        */
         parent.genChildren();
+        /*
+        After genChildren, Children Makespan is not correct and Fitness not.
+        */
         parent.calculateMakespan();
+        /*
+        After calculateMakespan, all population Makespan is corrent but Fitness NOT.
+        Here Makespan should be EXACTLY correct.
+        */
         parent.envSelection();
+        /*
+        After envSelection, Makespan keep correct but Fitness NOT.
+        */
+        parent.calculateFitness();
+        /*
+        After calculateFitness, both correct.
+        Here Fitness should be EXACTLY correct.
+        */
         parent.sortPopulation();
-
+        /*
+        After sortPopulation, we can get sorted-by-fitness makespan-correct population, fitness correct as well. 
+        */
         if (pitr % LOCAL_SEARCH_FREQUENCY == 0)
         {
             parent.localSearch(LOCAL_SEARCH_CHILDREN);
@@ -32,6 +53,9 @@ int main(int argc, char const *argv[])
         {
             parent.calculateFitness();
         }
+        /*
+        No matter what we do, Fitness SHOULD BE CORRECT HERE!!!!
+        */
     }
     return 0;
 }
